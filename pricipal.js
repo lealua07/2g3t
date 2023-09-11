686afcf999a736d91c76231f58195f4d071822f6
lealua07 <108945557+lealua07@users.noreply.github.com>
2023-09-11 11:43:36 -0300

    jgjf

--- pricipal.js
+++ pricipal.js
@@ -3,4 +3,10 @@ soma = (a,b) => a + b;
 sub = (a,b) => a - b;
 div = (a,b) => a / b;
 mult = (a,b) => a * b;
+raiz = a => Match . sqrt (a);
+eq2grau = (a,b,c) => {
+    if(a == 0) return "Não é uma equação de Segundo Grau.";
+   let delta = sub (mult(b.b),mult (4, mult (a,c)));
+   if(delta < 0) return "não possui raiz real.";
+}
 escrever( soma(9,soma (6,1) ));
